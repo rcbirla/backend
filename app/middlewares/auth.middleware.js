e13f28b48a8875969f673c716bc21d519fcda994
Rajendra Dhami <rajendra@Apollo-Rajendra-Dhamis-Mac.local>
2023-09-11 17:07:51 +0530

    Minor change

--- app/middlewares/auth.middleware.js
+++ app/middlewares/auth.middleware.js
@@ -3,24 +3,28 @@ const { tokenSecret } = require("../services/config.service");
 const User = require("../models/user.model");
 
 module.exports = (role) => async (req, res, next) => {
-  const { authorization } = req.headers;
-  const token = authorization.split(" ").pop();
-  if (!token) {
-    return res.status(401).send({ message: "Unauthorized!" });
+  try {
+    const { authorization } = req.headers;
+    const token = authorization?.split(" ").pop();
+    if (!token) {
+      return res.status(401).send({ message: "Unauthorized!" });
+    }
+    return jwt.verify(token, tokenSecret, async (err, decoded) => {
+      if (err) {
+        return res.status(401).send({ message: "Unauthorized!" });
+      }
+      const user = decoded.user;
+      if (!user) {
+        return res.status(401).send({ message: "Unauthorized!" });
+      }
+      const userDoc = await User.findById(user).select("-password");
+      if (userDoc && userDoc.role === role) {
+        req.user = userDoc;
+        return next();
+      }
+      return res.status(401).send({ message: "Unauthorized!" });
+    });
+  } catch (error) {
+    return res.status(500).send({ message: "Server error!" });
   }
-  return jwt.verify(token, tokenSecret, async (err, decoded) => {
-    if (err) {
-      return res.status(401).send({ message: "Unauthorized!" });
-    }
-    const user = decoded.user;
-    if (!user) {
-      return res.status(401).send({ message: "Unauthorized!" });
-    }
-    const userDoc = await User.findById(user).select("-password");
-    if (userDoc && userDoc.role === role) {
-      req.user = userDoc;
-      return next();
-    }
-    return res.status(401).send({ message: "Unauthorized!" });
-  });
 };
